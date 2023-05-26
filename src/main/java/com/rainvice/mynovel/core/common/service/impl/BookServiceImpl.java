d62cae903236a1b2cca0db3329cfacbe4bf9fbdb
Cheng <2379367401@qq.com>
2023-05-26 15:01:09 +0800

    feat(pom.xml,application.yml, logback-spring.xml): 暂时删除serurity依赖，增加logback和knife4j配置信息
    增加了logback，并设置了dev场景
    增加了knife4j接口文档，地址：http://localhost:8888/doc.html

---
+++ src/main/java/com/rainvice/mynovel/core/common/service/impl/BookServiceImpl.java
@@ -0,0 +1,11 @@
+package com.rainvice.mynovel.core.common.service.impl;
+
+import com.rainvice.mynovel.core.common.service.BookService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookServiceImpl implements BookService {
+
+
+
+}
